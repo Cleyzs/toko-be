6d5de835881ad82f677fbb23c4ef5f999614f6c9
cleyzs <clemenpros@gmail.com>
2025-03-20 18:37:38 +0700

    migration user

--- config/config.js
+++ config/config.js
@@ -1,11 +1,13 @@
+// config/config.js
 require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
+    username: process.env.DB_USER,
+    password: process.env.DB_PASS,
     database: process.env.DB_NAME,
     host: process.env.DB_HOST,
-    dialect: "postgres"
+    port: process.env.DB_PORT,
+    dialect: 'postgres'
   }
 };
