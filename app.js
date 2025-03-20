f7ff2263dbdd72a94928e272934c22c79f5b5143
BoflyEta <bofly321@gmail.com>
2025-03-20 17:50:55 +0700

    fix: init project with controller example

--- app.js
+++ app.js
@@ -1,20 +1,18 @@
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const userRoutes = require('./routes/userRoutes');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-
-var app = express();
-
-app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+// Routes
+app.get('/', (req, res) => {
+  res.send('Welcome to Express + Sequelize + PostgreSQL!');
+});
 
-module.exports = app;
+app.use('/user', userRoutes);
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`🚀 Server running on http://localhost:${PORT}`);
+});
---
+++ controllers/userController.js
@@ -0,0 +1,23 @@
+// controllers/userController.js
+const { User } = require('../models');
+
+// GET /user - get all users
+exports.getUsers = async (req, res) => {
+  try {
+    const users = await User.findAll();
+    res.json(users);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
+
+// POST /user - create new user
+exports.createUser = async (req, res) => {
+  const { name, email, password } = req.body;
+  try {
+    const newUser = await User.create({ name, email, password });
+    res.status(201).json(newUser);
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+};
